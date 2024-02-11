8f2a7839d156af546306b79871c7836af720c386
Jeff Spacek <Jeffrey.spacek@gmail.com>
2024-02-10 21:39:09 -0800

    feat added index.jsx file and content

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,7 +3,7 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
-import { Button, Form} from "react-bootstrap";
+//import { Button, Form} from "react-bootstrap";
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
