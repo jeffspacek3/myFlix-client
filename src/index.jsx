88374df62c591c74ddd2658315e2d13591776bbb
Jeff Spacek <Jeffrey.spacek@gmail.com>
2024-02-12 20:20:23 -0800

    feat added initial navigation-bar.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,18 @@
 import { createRoot } from "react-dom/client";
-import { MainView } from "./components/main-view/main-view"
+import { MainView } from "./components/main-view/main-view";
+import Container from "react-bootstrap/Container";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 
-// Import statement to indicate that you need to bundle `./index.scss`
-import "./index.scss";
-
-// Main component (will eventually use all the others)
 const MyFlixApplication = () => {
-  return < MainView />;
+  return (
+    <Container>
+      <MainView />
+    </Container>
+  );
 };
 
-// Finds the root of your app
 const container = document.querySelector("#root");
 const root = createRoot(container);
-
-// Tells React to render your app in the root DOM element
 root.render(<MyFlixApplication />);
